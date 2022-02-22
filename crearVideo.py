46e170e08dbe855830a0b6a7c2ba11892dc978d1
David Solanas <davidsolanas086@gmail.com>
2022-02-22 13:19:01 +0100

    establecer tiempo de imagen en función del ritmo de la canción

--- crearVideo.py
+++ crearVideo.py
@@ -2,10 +2,27 @@ import numpy
 from moviepy.editor import *
 import glob
 import os
+
+# pip install librosa
+import librosa
+
+
+
+def get_beat_times(audio_file):
+  y, sr = librosa.load(audio_file) 
+
+  # approach 1 - onset detection and dynamic programming
+  tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+  # beats now contains the beat *frame positions*
+  # convert to timestamps like this:
+  beat_times = librosa.frames_to_time(beats, sr=sr)
+
+  return beat_times
+
 if __name__ == "__main__":
     #Mira a ver si ves una forma menos cutre de hacer esto
     imagenes = glob.glob("./videos/*.jpg")
-    imagenes2 = glob.glob("./videos/*.jpg")
+    imagenes2 = glob.glob("./videos/*.jpeg")
 
 
     #Esta parte del codigo la he hecho para ver que proporciones hay en los distintos videos
@@ -52,12 +69,31 @@ if __name__ == "__main__":
     print("Imagenes ",len(imagenes),i)  
     print("Creando clips de imagen")
     #Estaria guapo meterle a cada clip un texto encima con un id unico, asi si hay que escalar alguno o rotarlo es mas facil
-    clips = [ImageClip(m).set_duration(0.1)
-      for m in imagenes[:20]]#Con las 20 primeras imagenes ya me va como una mierda y son 600 y pico
+    clips = []
+
+    beat_times = get_beat_times('macklemore.wav')
+
+    for i, m in enumerate(imagenes):
+      c = ImageClip(m)
+      w, h = c.size
+      if(w > h):
+        ratio = float(w)/float(h)
+      else:
+        ratio = float(h)/float(w)
+      
+      dur = 10 if i == len(imagenes) - 1 else beat_times[i+1] - beat_times[i]
+      clips.append(c.set_duration(2*dur))
+
+
+    #clips = [ImageClip(m).resize(0.5).set_duration(0.1) for m in imagenes] #Con las 20 primeras imagenes ya me va como una mierda y son 600 y pico
     print("Creados clip de imagen")
     print("Concatenando")
     #Esto estoy seguro que tiene opciones para que vata mas rapido
     concat_clip = concatenate_videoclips(clips, method="compose")
+
+    audioclip = AudioFileClip("macklemore.wav")
+    concat_clip = concat_clip.set_audio(audioclip)
+
     print("Concatenado")
     print("Escribiendo")
     #24 fps creo que son muchos para imagenes aunque hay que meter fragmentos de video tambien
