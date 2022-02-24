a383913023f5d889f769c7d56db279199a0744fa
David Solanas <davidsolanas086@gmail.com>
2022-02-24 18:07:23 +0100

    organizar codigo

--- crearVideo.py
+++ crearVideo.py
@@ -19,29 +19,8 @@ def get_beat_times(audio_file):
 
   return beat_times
 
-if __name__ == "__main__":
-    #Mira a ver si ves una forma menos cutre de hacer esto
-    imagenes2 = glob.glob("./videos/*.jpeg")
-    imagenes = glob.glob("./videos/*.jpg") + imagenes2
 
-    videos = glob.glob("./videos/noChill/videos/*")
-    #videos = glob.glob("./videos/*.mp4") + videos2
-    print(len(videos), len(imagenes))
-
-    #Esta parte del codigo la he hecho para ver que proporciones hay en los distintos videos
-    # y como se repiten por si hay que hacer preprocesado y luego usar el comando ffmpeg
-    
-
-    #Aqui se acaba la parte pensada para preProcesado
-
-    #Esta parte seria lo ideal, ya que se la suda el tamanio de las disintas imagnes
-    # (se podria escalar la que haga falta)
-    # Lo malo es que a mi me usa muchiiiiiiisima RAM para la mierda que es realmente
-    # Esto tambien nos permitiria customizar la duracion de cada imagen por si queremeos que alguna dure
-    # mas cuando rompa la cancion y tal
-    #Igual en lugar de crear todos los clips de imagen a las bravas conviene mas crear uno y meterlo en uno de video directamente y
-    # liberar la memoria de ese clip de imagen
-    
+def crear_video(videos, outfile):
     print("Creando clips de imagen")
     #Estaria guapo meterle a cada clip un texto encima con un id unico, asi si hay que escalar alguno o rotarlo es mas facil
     clips = []
@@ -50,21 +29,6 @@ if __name__ == "__main__":
     beat_times = get_beat_times('overture.wav')
     # Obtener la duración media entre beats
     mean_dur = numpy.mean(numpy.gradient(beat_times))
-    '''
-    for i, m in enumerate(imagenes):
-      c = ImageClip(m)
-      w, h = c.size
-      if(w > h):
-        ratio = float(w)/float(h)
-      else:
-        ratio = float(h)/float(w)
-
-      #c.resize( (1920,2080) )
-      dur = 10 if i == len(imagenes) - 1 else beat_times[i+1] - beat_times[i]
-      
-      clips.append(c.resize( height=1080 ).set_duration(0.1))
-    '''
-    clips = []
 
     for i, v in enumerate(videos):
       filename = os.path.basename(v).split('.')[0]
@@ -89,9 +53,63 @@ if __name__ == "__main__":
     print("Concatenado")
     print("Escribiendo")
     #24 fps creo que son muchos para imagenes aunque hay que meter fragmentos de video tambien
-    concat_clip.write_videofile("test.mp4", fps=24)
+    concat_clip.write_videofile(outfile, fps=24)
     print("Escrito")
     
     print("Hola mamba!")
     #Aqui se acaba la mejor opcion
+
+
+def crear_video_imagenes(imagenes, audio, outfile):
+    print("Creando clips de imagen")
+    #Estaria guapo meterle a cada clip un texto encima con un id unico, asi si hay que escalar alguno o rotarlo es mas facil
+    clips = []
+
+    # Obtener timestamps de los beats de la canción
+    beat_times = get_beat_times(audio)
+    # Obtener la duración media entre beats
+    mean_dur = numpy.mean(numpy.gradient(beat_times))
+    
+    for i, m in enumerate(imagenes):
+      c = ImageClip(m)
+      w, h = c.size
+      if(w > h):
+        ratio = float(w)/float(h)
+      else:
+        ratio = float(h)/float(w)
+
+      dur = 10 if i == len(imagenes) - 1 else beat_times[i+1] - beat_times[i]
+      
+      clips.append(c.resize( height=1080 ).set_duration(0.1))
+    
+
+    #clips = [ImageClip(m).resize(0.5).set_duration(0.1) for m in imagenes] #Con las 20 primeras imagenes ya me va como una mierda y son 600 y pico
+    print("Creados clip de imagen")
+    print("Concatenando")
+    #Esto estoy seguro que tiene opciones para que vata mas rapido
+    concat_clip = concatenate_videoclips(clips, method="compose")
+
+    audioclip = AudioFileClip(audio)
+    concat_clip = concat_clip.set_audio(audioclip)
+
+    print("Concatenado")
+    print("Escribiendo")
+    #24 fps creo que son muchos para imagenes aunque hay que meter fragmentos de video tambien
+    concat_clip.write_videofile(outfile, fps=24)
+    print("Escrito")
+    
+    print("Hola mamba!")
+    #Aqui se acaba la mejor opcion
+
+if __name__ == "__main__":
+    #Mira a ver si ves una forma menos cutre de hacer esto
+    noChill = glob.glob("./videos/noChill/*.*")
+    
+    chill = glob.glob("./videos/chill/*.*")
+
+    videos = glob.glob("./videos/noChill/videos/*")
+
+    crear_video(videos, 'video_no_chill.mp4')
+
+    crear_video_imagenes(chill, 'amanecer.wav', 'video_chill.mp4')
     
