145c8d0eda31e1056f4df82e0864cc21ba4b1238
David Solanas <davidsolanas086@gmail.com>
2022-02-24 14:28:50 +0100

    generar video

--- crearVideo.py
+++ crearVideo.py
@@ -21,41 +21,17 @@ def get_beat_times(audio_file):
 
 if __name__ == "__main__":
     #Mira a ver si ves una forma menos cutre de hacer esto
-    imagenes = glob.glob("./videos/*.jpg")
     imagenes2 = glob.glob("./videos/*.jpeg")
+    imagenes = glob.glob("./videos/*.jpg") + imagenes2
 
+    videos = glob.glob("./videos/noChill/videos/*")
+    #videos = glob.glob("./videos/*.mp4") + videos2
+    print(len(videos), len(imagenes))
 
     #Esta parte del codigo la he hecho para ver que proporciones hay en los distintos videos
     # y como se repiten por si hay que hacer preprocesado y luego usar el comando ffmpeg
-    dict  =  {}
-    for imagen in imagenes2:
-        imagenes.append(imagen)
     
-    i = 0
-    for imagen in imagenes:
-      print(i,len(imagenes))
-      clip = ImageClip(imagen)
-      w,h = clip.size
-      if (w,h) in dict.keys():
-        dict[(w,h)]+=1
-      else:
-        dict[(w,h)] = 1
-      i+=1
-    print(dict)
-    dic_ratio = {}
-    for key in dict.keys():
-      w,h = key
-      if(w > h):
-        ratio = float(w)/float(h)
-      else:
-        ratio = float(h)/float(w)
-      str_key = "{:.1f}".format(ratio)
-      if str_key in dic_ratio.keys():
-        dic_ratio[str_key]+=1
-      else:
-        dic_ratio[str_key] = 1
 
-    print(dic_ratio)
     #Aqui se acaba la parte pensada para preProcesado
 
     #Esta parte seria lo ideal, ya que se la suda el tamanio de las disintas imagnes
@@ -66,13 +42,15 @@ if __name__ == "__main__":
     #Igual en lugar de crear todos los clips de imagen a las bravas conviene mas crear uno y meterlo en uno de video directamente y
     # liberar la memoria de ese clip de imagen
     
-    print("Imagenes ",len(imagenes),i)  
     print("Creando clips de imagen")
     #Estaria guapo meterle a cada clip un texto encima con un id unico, asi si hay que escalar alguno o rotarlo es mas facil
     clips = []
 
-    beat_times = get_beat_times('macklemore.wav')
-
+    # Obtener timestamps de los beats de la canción
+    beat_times = get_beat_times('overture.wav')
+    # Obtener la duración media entre beats
+    mean_dur = numpy.mean(numpy.gradient(beat_times))
+    '''
     for i, m in enumerate(imagenes):
       c = ImageClip(m)
       w, h = c.size
@@ -80,10 +58,24 @@ if __name__ == "__main__":
         ratio = float(w)/float(h)
       else:
         ratio = float(h)/float(w)
-      
-      dur = 10 if i == len(imagenes) - 1 else beat_times[i+1] - beat_times[i]
-      clips.append(c.set_duration(2*dur))
 
+      #c.resize( (1920,2080) )
+      dur = 10 if i == len(imagenes) - 1 else beat_times[i+1] - beat_times[i]
+      
+      clips.append(c.resize( height=1080 ).set_duration(0.1))
+    '''
+    clips = []
+
+    for i, v in enumerate(videos):
+      filename = os.path.basename(v).split('.')[0]
+      aux = filename.split('_')
+      start = int(aux[1])
+      dur = 1.5*mean_dur if len(aux) == 2 else 2*mean_dur
+
+      v = VideoFileClip(v)
+      if i == len(videos) - 1:
+        dur = 6
+      clips.append(v.resize( height=1080 ).subclip(start).set_duration(dur))
 
     #clips = [ImageClip(m).resize(0.5).set_duration(0.1) for m in imagenes] #Con las 20 primeras imagenes ya me va como una mierda y son 600 y pico
     print("Creados clip de imagen")
@@ -91,7 +83,7 @@ if __name__ == "__main__":
     #Esto estoy seguro que tiene opciones para que vata mas rapido
     concat_clip = concatenate_videoclips(clips, method="compose")
 
-    audioclip = AudioFileClip("macklemore.wav")
+    audioclip = AudioFileClip("overture.wav")
     concat_clip = concat_clip.set_audio(audioclip)
 
     print("Concatenado")
@@ -103,6 +95,3 @@ if __name__ == "__main__":
     print("Hola mamba!")
     #Aqui se acaba la mejor opcion
     
-    #Esto se supone que hace lo de arriba pero pide que ssean todas del mismo tamanio y tampoco nos deja customizar mucho
-    clip = ImageSequenceClip("./videos/", fps=10)
-    clip.write_videofile("result.mp4")
